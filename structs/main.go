678fd1229edb81860de1050aaa838d8908ce51e8
Rafael Eloy Boemer <rafael.boemer@hbsis.com.br>
2021-08-31 22:04:14 -0300

    Working with pointers and its shortcuts

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,39 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	var alex person
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	//jimPointer := &jim // '&' this operator is getting the variable address and assigning it to a new variable
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// we can gain the same result doing this...
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+// '*' this operator in front a type means that we're expecting the type 'person' from a pointer
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName // '*' this operator is getting the value from the pointer address
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
